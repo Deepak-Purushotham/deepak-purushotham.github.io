102476638f86ac20d81e8e370381531ac7acc14f
Deepak-Purushotham <deepak_purushotham@outlook.com>
2026-06-02 11:01:34 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('Business Analyst website ready');
+
+/* =========================
+FADE IN ON SCROLL
+========================= */
+
+const animatedElements = document.querySelectorAll(
+'.timeline-card, .skill-card, .impact-card, .education-card, .contact-card'
+);
+
+const observer = new IntersectionObserver((entries) => {
+
+entries.forEach((entry) => {
+
+  if (entry.isIntersecting) {
+    entry.target.classList.add('show');
+  }
+
+});
+
+}, {
+threshold: 0.15
+});
+
+animatedElements.forEach((element) => {
+observer.observe(element);
+});
+
 });
